da9a4a177bb067cdad6fad229e3da30919405ffb
SkyYongFly <yongzhu2014@outlook.com>
2018-10-10 17:31:57 +0800

    Spring Boot Admin集成到注册中心

--- spring-boot-admin-server/src/main/java/com/example/adminserver/SpringBootAdminServerApplication.java
+++ spring-boot-admin-server/src/main/java/com/example/adminserver/SpringBootAdminServerApplication.java
@@ -3,10 +3,13 @@ package com.example.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @EnableAdminServer 注解启动Admin服务端
+ * @EnableDiscoveryClient 注册到服务注册中心
  */
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class SpringBootAdminServerApplication {
